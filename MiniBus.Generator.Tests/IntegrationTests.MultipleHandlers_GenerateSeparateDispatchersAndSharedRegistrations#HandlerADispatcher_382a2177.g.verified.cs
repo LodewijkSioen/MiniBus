bb845e20bf7fde5f928c6e502ae08b2a4f1c462e
Lodewijk Sioen <lodewijksioen@gmail.com>
2026-05-23 09:14:23 +0200

    Implemented OTEL span (#10)

--- MiniBus.Generator.Tests/IntegrationTests.MultipleHandlers_GenerateSeparateDispatchersAndSharedRegistrations#HandlerADispatcher_382a2177.g.verified.cs
+++ MiniBus.Generator.Tests/IntegrationTests.MultipleHandlers_GenerateSeparateDispatchersAndSharedRegistrations#HandlerADispatcher_382a2177.g.verified.cs
@@ -16,6 +16,8 @@ namespace TestApp
             _handler = handler;
         }
 
+        public string HandlerName => nameof(global::TestApp.HandlerA);
+
         public async global::System.Threading.Tasks.Task<
             global::MiniBus.Result<global::TestApp.HandlerA.Response>>
             Handle(global::TestApp.HandlerA.Request request)
